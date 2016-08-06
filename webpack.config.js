950bfca67cb5c62e51f17f32627138b904e7858a
tally-forth <matt@tally.us>
2016-08-06 10:37:36 -0400

    Add NODE_ENV and process.env.NODE_ENV to webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,47 @@
-var debug = process.env.NODE_ENV !== "production";
+var isProd = (process.env.NODE_ENV === 'production');
 var LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 var webpack = require('webpack');
+console.log('isProd', isProd);
+
 var path = require('path');
-var StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin');
 var staticContent = require('./src/static_site/static.js');
+var StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin');
+
+// Conditionally return a list of plugins to use based on the current environment. Repeat this pattern for any other config key (ie: loaders, etc).
+function getPlugins() {
+  var plugins = [];
+  // Always expose NODE_ENV to webpack, you can now use `process.env.NODE_ENV`
+  // inside your code for any environment checks; UglifyJS will automatically drop any unreachable code.
+  // plugins.push(
+  //   new webpack.DefinePlugin({
+  //   "process.env": {
+  //     NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+  //   }
+  // }));
+
+  // Conditionally add plugins for Production builds.
+  if (isProd) {
+    // plugins.push(new webpack.optimize.UglifyJsPlugin());
+    plugins.push(
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+      new StaticSiteGeneratorPlugin('routes.min.js', staticContent.routes, staticContent),
+      new LodashModuleReplacementPlugin({'collections': true}),
+      new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/)
+    )
+  }
+
+  // Conditionally add plugins for Development
+  else {
+  }
+  return plugins;
+}
+
 
 module.exports = {
   context: path.join(__dirname, "src"),
-  devtool: debug ? "inline-sourcemap" : null,
+  devtool: isProd ? null : "inline-sourcemap",
   entry: "./js/routes.js",
   module: {
     loaders: [
@@ -26,17 +60,11 @@ module.exports = {
     path: __dirname + "/src/",
     filename: "routes.min.js"
   },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-    new StaticSiteGeneratorPlugin('routes.min.js', staticContent.routes, staticContent),
-    new LodashModuleReplacementPlugin({
-      'collections': true,
-    }),
-    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/)
-  ],
+  plugins: getPlugins(),
   devServer: {
     historyApiFallback: true
   }
 };
+
+
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
